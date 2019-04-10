ff6a3167ed86542577add00541b7be2bcb61f6dc
Boy-yang <yang-zhi-xiang@outlook.com>
2019-04-11 02:19:24 +0800

    fix:tsconfig.json

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,24 +1,41 @@
 import React,{Component} from 'react';
-import './Header.scss'
 import { Link } from 'react-router-dom';
+import {Input} from 'antd';
+import './Header.scss'
+
+const Search=Input.Search;
 
 class Header extends Component {
     constructor ( props ) {
         super( props );
     }
 
+    handleSearch(value){
+        console.log(value)
+    }
+
     render() {
         return (
-            <div className="Header">
-                <a href="#" className='logo'>文轩网</a>
+            <div className="header">
+                <div className='logo'>Blog</div>
+                <div className='nav'>
+                    <a href='javascript:;'>首页</a>
+                    <a href='javascript:;'>技术</a>
+                    <a href='javascript:;'>生活</a>
+                    <a href='javascript:;'>关于我</a>
+                </div>
                 <div className='search'>
-                    <input type="text" className={ 'index-search' } placeholder={ '' } />
-                    <a href="javascript:;" />
-                    <i className="fa fa-search" aria-hidden="true" />
+                    <Search
+                        placeholder="input search text"
+                        onSearch={value => this.handleSearch(value)}
+                        enterButton
+                    />
                 </div>
-                <div className="user">
-                    <Link to='/login'>登录</Link>
-                </div>
+                <div className='write-login'>
+                    <a href='javascript:;'>写文章</a>
+                    &nbsp;&nbsp;|&nbsp;&nbsp;
+                    <Link to='/login'>登陆</Link>
+                </div> 
             </div>
         );
     }
